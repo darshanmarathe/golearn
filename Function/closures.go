239a8175978ad9a3bdf809122288b32c1ebde381
D24809 <darshanmarathe@gmail.com>
2017-06-01 12:05:20 +0530

    Added functional stuff and closures

---
+++ Function/closures.go
@@ -0,0 +1,34 @@
+package main 
+
+import (
+	"fmt"
+)
+
+
+
+type PrinterFunc func(string) ()
+
+func main(){
+	 Concatnitor := Concat("Hello")
+	Concatnitor(" World")
+	Concatnitor(" this")
+	Concatnitor(" is")
+	Concatnitor(" go")
+	Concatnitor(" language")
+	Concatnitor(" showing closures")
+}
+
+
+
+func Concat(s string)(func(ar string)){
+
+	var line = s
+	return func(z string){
+		line += z
+		fmt.Println(line)
+	}
+
+
+}
+
+
