03a3ff501045706d1a10d13499821b27425120c7
Marathe <darshan.marathe@philips.com>
2021-04-14 19:50:02 +0530

    work in progress on gocmds

--- gocmds/build.bat
+++ gocmds/build.bat
@@ -1,3 +1,13 @@
-go run cat.go 
-go run ls.go 
-go run touch.go 
+cls
+go run cat.go
+pause & cls 
+go run cat.go "file.txt"
+pause & cls 
+go run cat.go "file.txt" -l
+pause & cls 
+go run cat.go "file.txt" -c
+pause & cls 
+go run cat.go "file.txt" -c -l
+pause & cls 
+@REM go run ls.go 
+@REM go run touch.go 
--- gocmds/cat.go
+++ gocmds/cat.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("cat the code")
+
+	Args := os.Args[1:]
+	if len(Args) == 0 {
+		printHelpMessage()
+		return
+	}
+	for _, v := range Args {
+		fmt.Println(v)
+	}
+}
+
+func printHelpMessage() {
+	fmt.Println("Looks like you did not use program correctly.")
+	fmt.Println("Try using program like.")
+	fmt.Println("cat filename.txt will print the file")
+	fmt.Println("cat filename.txt -l will print the file with lines")
+	fmt.Println("cat filename.txt -c will print and copy the content of the file to memory")
 }
